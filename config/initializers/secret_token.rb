564ea3730f3fc270be807e6b3a8aa4e7ebd2713f
Kym McInerney <kym@holodigm.com.au>
2014-03-04 22:23:29 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LandagentExample::Application.config.secret_key_base = 'df26da407ad1d91cd4a4d521d039509915d50f3ef50373b06100d7063d83b5b80769ffbef2cf7eef55f79a0fadd0457b090caff698e948eb102d88a26cd47b38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LandagentExample::Application.config.session_store :cookie_store, key: '_landagent-example_session'
